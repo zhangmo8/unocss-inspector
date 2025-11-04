0213013a8d9cb0faddb8eff6aec297e5a180727a
zhangmo8 <wegi866@gmail.com>
2025-11-04 15:25:19 +0800

    fix: lint

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'node:path'
-import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue()],
